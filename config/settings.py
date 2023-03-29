6a6a1490cb619807da997cf13aa3cf33cae20924
Jahongir Ibragimov <118691897+ChogirmaliYigit@users.noreply.github.com>
2023-03-29 21:21:49 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-chsi@aob#+_sgcv#av8kfvq&)i%cgp-#x%7)7o=l1$n-ew1dns'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
